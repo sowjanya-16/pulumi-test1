495d735caecc5a9cbc5312bea7b800037395f89f
Nimmagadda Venkata Sowjanya (MS/ECL5) <vmq1cob@bosch.com>
2024-03-07 17:36:00 +0530

    first commit

---
+++ Downloads/SmartLab_SourceCode 2/SmartLab_SourceCode/SmartLab/Controllers/MasterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LC_Reports_V1.Views.Master
+{
+    public class MasterController : Controller
+    {
+        // GET: Master
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
